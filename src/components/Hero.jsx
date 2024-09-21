dffc0a36bfed076a166a79d5f778f9e5b95ceafe
Harsh-Nikam07 <134618980+Harsh-Nikam07@users.noreply.github.com>
2024-09-21 21:20:51 +0530

    resume update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,10 +21,10 @@ const iconVariantsv2 = {
 };
 
 const onbuttonClick = () => {
-    const pdfUrl = "/Harsh NikamCV(28-08-2024).pdf"; 
+    const pdfUrl = "/HarshNikamCV(21-09-2024).pdf"; 
     const link = document.createElement("a");
     link.href = pdfUrl;
-    link.download = "Harsh NikamCV(28-08-2024).pdf"; 
+    link.download = "Harsh NikamCV(21-09-2024).pdf"; 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
